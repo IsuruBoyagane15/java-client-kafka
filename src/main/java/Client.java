a0d006500760d8fd6ba87b2f5b9c6955dcda2740
isuruboyagane15 <isuruboyagane15@gmail.com>
2019-07-26 13:45:38 +0530

    after consensus threads terminate properly

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,12 +1,10 @@
-import java.util.Scanner;
-
-import com.itextpdf.kernel.counter.SystemOutEventCounter;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
+import javax.script.Bindings;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 
@@ -14,6 +12,7 @@ public class Client{
 
     private String kafkaServer = "localhost:9092";
     private static String topic = "con";
+    private Boolean consensusAchieved = false;
     private String code;
     private String evaluation;
     private String clientId;
@@ -37,13 +36,21 @@ public class Client{
         ScriptEngine nashormEngine = new ScriptEngineManager().getEngineByName("nashorn");
 
         try {
-            while (true) {
-                ConsumerRecords<String, String> records = this.kafkaConsumer.poll(100);
+            while (this.consensusAchieved == false) {
+                ConsumerRecords<String, String> records = this.kafkaConsumer.poll(10);
                 for (ConsumerRecord<String, String> record : records) {
+
                     this.code += record.value();
-//                    System.out.println(record.partition());
-                    System.out.println(nashormEngine.eval(this.code + this.evaluation));
-//                    System.out.println((this.code + this.evaluation));
+                    Object result = nashormEngine.eval(this.code + this.evaluation);
+
+                    int integerResult = (Integer)result;
+                    if (integerResult == 1){
+                        this.consensusAchieved = true;
+                        System.out.println("Consensus.");
+                    }
+                    else{
+                        System.out.println(false);
+                    }
 
                 }
             }
@@ -58,16 +65,14 @@ public class Client{
     }
 
     public static void main(String[] args) {
+
         final Client client = new Client(args[0]);
         System.out.println(client.clientId);
 
 
-//        client.consumeMessage();
-
-//        assume the consensus scenario is a leader election
 //        assume the number of node is 3
         client.code = "var x=null;y=null;z=null;";
-        client.evaluation = "if(x===y && y===z){true;}else{false;}";
+        client.evaluation = "if(x===y && y===z && x!==null){1;}else{0;}";
 //        int clientsCount = 3;
 //
 
@@ -84,17 +89,24 @@ public class Client{
         Runnable producing = new Runnable() {
             @Override
             public void run() {
-                Scanner scanner = new Scanner(System.in);
-                while(true){
-                    String clientValue = scanner.next();
+
+                while(client.consensusAchieved == false){
+
+                    int clientRandom = (int)(1 + Math.random()*3);
+                    System.out.println(clientRandom);
+                    String clientValue = client.clientId + "=" + Integer.toString(clientRandom) + ";";
                     client.produceMessages(clientValue);
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
 
                 }
             }
         };
         new Thread(producing).start();
-
-//
 
     }
 }
